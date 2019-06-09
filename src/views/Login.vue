3f0c12318fa066a6c29282a1040eab91178c814a
James Martin <jamesmartin442@gmail.com>
2019-06-09 12:55:08 +0100

    Vue cli wants weird prefixes, ok

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,9 +1,9 @@
 <template>
-    <div class="login-form">
-        <form>
-			<v-btn color="info" @click="login">Log in with Facebook</v-btn>
-        </form>
-    </div>
+  <div class="login-form">
+    <form>
+      <v-btn color="info" @click="login">Log in with Facebook</v-btn>
+    </form>
+  </div>
 </template>
 
 <script>
@@ -16,18 +16,20 @@ export default {
 	},
 	methods: {
 		login() {
-			window.location.href = `${process.env.API_ADDRESS}/auth/facebook?returnTo=${process.env.LOCAL_ADDRESS}`;
+			window.location.href = `${
+				process.env.VUE_APP_API_ADDRESS
+			}/auth/facebook?returnTo=${process.env.VUE_APP_LOCAL_ADDRESS}`;
 		}
 	}
 };
 </script>
 
 <style lang="scss" scoped>
-	.login-form {
-		height: 100vh;
-		width: 100vw;
-		display: flex;
-		justify-content: center;
-		align-items: center;
-	}
+  .login-form {
+    height: 100vh;
+    width: 100vw;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
 </style>
