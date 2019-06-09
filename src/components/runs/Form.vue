21731e0dc328807916edc2b7c6bbce9001362a66
James Martin <jamesmartin442@gmail.com>
2019-06-09 15:33:39 +0100

    Computed prop

--- src/components/runs/Form.vue
+++ src/components/runs/Form.vue
@@ -1,157 +1,120 @@
 <template>
-	<div v-if="loading">
-		Loading...
-	</div>
-	<v-form v-else>
-		<v-container fluid>
-			<v-layout row>
-				<v-flex xs12>
-					<Separator>Booked run</Separator>
-					<DogSelector v-model="model.dogId" />
-					<HandlerSelector v-model="model.handlerOverride" />
-				</v-flex>
-			</v-layout>
-			<v-layout row wrap>
-				<v-flex xs12 md4>
-					<v-text-field type="number" label="Class number" v-model="model.classNumber" />
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-text-field label="Judge" v-model="model.judge" />
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-menu
-						:close-on-content-click="false"
-						:nudge-right="40"
-						lazy
-						transition="scale-transition"
-						offset-y
-						full-width
-						min-width="290px"
-					>
-						<v-text-field
-							slot="activator"
-							label="Run date"
-							prepend-icon="event"
-							readonly
-							v-model="model.date"
-						/>
-						<v-date-picker
-							v-model="model.date"
-							:max="show.endDate"
-							:min="show.startDate"
-							scrollable
-						/>
-					</v-menu>
-				</v-flex>
-			</v-layout>
-			<v-layout row wrap>
-				<v-flex xs12 md4>
-					<v-select
-						:items="classTypes"
-						item-text="label"
-						item-value="value"
-						label="Class type"
-						v-model="model.type"
-					/>
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-text-field
-						label="Type of special class"
-						v-model="model.specialType"
-						:disabled="model.type !== 'special'"
-					/>
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-select
-						:items="gradedOrCombined"
-						item-text="label"
-						item-value="value"
-						label="Graded or combined"
-						v-model="model.gradeType"
-					/>
-				</v-flex>
-			</v-layout>
-			<v-layout row wrap>
-				<v-flex xs12 md6>
-					<v-text-field
-						label="Grade"
-						v-model="model.grade"
-					/>
-				</v-flex>
-				<v-flex xs12 md6>
-					<v-text-field
-						label="Grade at the time"
-						v-model="model.currentGrade"
-					/>
-				</v-flex>
-			</v-layout>
+  <div v-if="loading">Loading...</div>
+  <v-form v-else>
+    <v-container fluid>
+      <v-layout row>
+        <v-flex xs12>
+          <Separator>Booked run</Separator>
+          <DogSelector v-model="model.dogId"/>
+          <HandlerSelector v-model="model.handlerOverride"/>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex xs12 md4>
+          <v-text-field type="number" label="Class number" v-model="model.classNumber"/>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-text-field label="Judge" v-model="model.judge"/>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-menu
+            :close-on-content-click="false"
+            :nudge-right="40"
+            lazy
+            transition="scale-transition"
+            offset-y
+            full-width
+            min-width="290px"
+          >
+            <v-text-field
+              slot="activator"
+              label="Run date"
+              prepend-icon="event"
+              readonly
+              v-model="model.date"
+            />
+            <v-date-picker
+              v-model="model.date"
+              :max="show.endDate"
+              :min="show.startDate"
+              scrollable
+            />
+          </v-menu>
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex xs12 md4>
+          <v-select
+            :items="classTypes"
+            item-text="label"
+            item-value="value"
+            label="Class type"
+            v-model="model.type"
+          />
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-text-field
+            label="Type of special class"
+            v-model="model.specialType"
+            :disabled="model.type !== 'special'"
+          />
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-select
+            :items="gradedOrCombined"
+            item-text="label"
+            item-value="value"
+            label="Graded or combined"
+            v-model="model.gradeType"
+          />
+        </v-flex>
+      </v-layout>
+      <v-layout row wrap>
+        <v-flex xs12 md6>
+          <v-text-field label="Grade" v-model="model.grade"/>
+        </v-flex>
+        <v-flex xs12 md6>
+          <v-text-field label="Grade at the time" v-model="model.currentGrade"/>
+        </v-flex>
+      </v-layout>
 
-			<Separator>Running order for run</Separator>
-			<v-layout row wrap>
-				<v-flex xs12 md4>
-					<v-text-field
-						label="Ring number"
-						v-model="model.ringNumber"
-					/>
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-text-field
-						label="Running order"
-						v-model="model.runningOrder"
-					/>
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-text-field
-						type="number"
-						label="Class size"
-						v-model="model.classSize"
-					/>
-				</v-flex>
-			</v-layout>
+      <Separator>Running order for run</Separator>
+      <v-layout row wrap>
+        <v-flex xs12 md4>
+          <v-text-field label="Ring number" v-model="model.ringNumber"/>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-text-field label="Running order" v-model="model.runningOrder"/>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-text-field type="number" label="Class size" v-model="model.classSize"/>
+        </v-flex>
+      </v-layout>
 
-			<Separator>After run</Separator>
-			<v-layout row wrap>
-				<v-flex xs12 md4>
-					<v-text-field
-						label="Course length"
-						v-model="model.courseLength"
-					/>
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-text-field
-						label="Course time"
-						v-model="model.courseTime"
-					/>
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-text-field
-						label="Winning time"
-						v-model="model.winningTime"
-					/>
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-text-field
-						type="number"
-						label="Your place"
-						v-model="model.place"
-					/>
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-text-field
-						label="Faults"
-						v-model="model.faults"
-					/>
-				</v-flex>
-				<v-flex xs12 md4>
-					<v-text-field
-						label="Your time"
-						v-model="model.runTime"
-					/>
-				</v-flex>
-			</v-layout>
-			<v-btn block color="success" @click="submit">Save</v-btn>
-		</v-container>
-	</v-form>
+      <Separator>After run</Separator>
+      <v-layout row wrap>
+        <v-flex xs12 md4>
+          <v-text-field label="Course length" v-model="model.courseLength"/>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-text-field label="Course time" v-model="model.courseTime"/>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-text-field label="Winning time" v-model="model.winningTime"/>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-text-field type="number" label="Your place" v-model="model.place"/>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-text-field label="Faults" v-model="model.faults"/>
+        </v-flex>
+        <v-flex xs12 md4>
+          <v-text-field label="Your time" v-model="model.runTime"/>
+        </v-flex>
+      </v-layout>
+      <v-btn block color="success" @click="submit">Save</v-btn>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
@@ -187,7 +150,7 @@ export default {
 			];
 		},
 		showId() {
-			return this.getShowId();
+			return this.$route.params.showId;
 		},
 		show() {
 			return this.$store.getters['shows/byId'](this.showId);
@@ -200,9 +163,6 @@ export default {
 		this.$store.dispatch('shows/loadShows');
 	},
 	methods: {
-		getShowId() {
-			return this.$route.params.showId;
-		},
 		submit() {
 			this.$emit('saved', this.model);
 		},
@@ -227,7 +187,7 @@ export default {
 				ringNumber: this.run.ringNumber || null,
 				runTime: this.run.runTime || null,
 				runningOrder: this.run.runningOrder || null,
-				showId: this.run.showId || this.getShowId() || null,
+				showId: this.run.showId || this.showId || null,
 				specialType: this.run.specialType || null,
 				type: this.run.type || null,
 				winningTime: this.run.winningTime || null
